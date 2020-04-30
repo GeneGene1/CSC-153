db8b63f858b3f96be2f5ce4328ce7779804f035f
GeneGene1 <47635902+GeneGene1@users.noreply.github.com>
2020-04-29 20:19:16 -0400

    Add files via upload

---
+++ RetailPriceCalculator/ConsoleUI/Program.cs
@@ -0,0 +1,47 @@
+﻿/* 
+ * 2.24.20
+ * CSC 153
+ * Eugene Davis-Paulino
+ * Creating a program that calculating the price of an item
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            double markup;
+            double wholesale;
+            double retail;
+
+            Console.WriteLine(ClassLibrary.StandardMessages.AskForMarkup());
+            input = Console.ReadLine();
+
+            Double.TryParse(input, out markup);
+
+            Console.WriteLine(ClassLibrary.StandardMessages.AskForCost());
+            input = Console.ReadLine();
+
+            Double.TryParse(input, out wholesale);          
+
+            retail = CalculateRetail(wholesale, markup);
+
+            Console.WriteLine(ClassLibrary.StandardMessages.DisplayRetail(retail));
+
+            Console.ReadLine();
+        }
+        public static double CalculateRetail(double wholesale, double markup)
+        {
+            double retail = wholesale * (markup / 100) + wholesale;
+            return retail;
+        }
+    }
+}
