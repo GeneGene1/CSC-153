94cdfaf906984dafcf4f1847a7d1608d800dacd7
GeneGene1 <47635902+GeneGene1@users.noreply.github.com>
2020-04-29 20:14:35 -0400

    Add files via upload

---
+++ FallingDistance/ClassLibrary/StandardMessages.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public static class StandardMessages
+    {
+        public static string FallingSecond()
+        {
+            return "How many second the object fall?";
+        }
+
+        public static string DisplayHeight(double height)
+        {
+            return $"The object is falling at {height} meters";
+        }
+    }
+}
