7651c35d16caed63a57b8d919705630d9d35548f
GeneGene1 <47635902+GeneGene1@users.noreply.github.com>
2020-04-29 20:01:51 -0400

    Add files via upload

---
+++ FallingDistance/ConsoleUI/Program.cs
@@ -0,0 +1,44 @@
+﻿/* 
+ * 2.24.20
+ * CSC 153
+ * Eugene Davis-Paulino
+ * Creating a program that calculate the distance of a falling object
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            double gravity = 9.8;
+            double time;
+            double height;
+
+            Console.WriteLine(ClassLibrary.StandardMessages.FallingSecond());
+            input = Console.ReadLine();
+
+            Double.TryParse(input, out time);
+
+            height = FallingDistance(gravity, time);
+
+            Console.WriteLine(ClassLibrary.StandardMessages.DisplayHeight(height));
+            input = Console.ReadLine();
+
+            Double.TryParse(input, out height);
+        }
+
+        public static double FallingDistance(double gravity, double time)
+        {
+            double distance = .5 * gravity * time * time;
+            return distance;
+        }
+    }
+}
